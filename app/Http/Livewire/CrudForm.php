4076019d465ac5b69812a3456b9656fc3cc788b9
ayran louro <ayranlouro@hotmail.com>
2021-05-10 15:28:30 -0300

    second:frontend:forms

--- app/Http/Livewire/CrudForm.php
+++ app/Http/Livewire/CrudForm.php
@@ -5,7 +5,8 @@ namespace App\Http\Livewire;
 use Livewire\Component;
 
 class CrudForm extends Component
-{
+{   
+    public $var;
     public function render()
     {
         return view('livewire.crud-form');
--- resources/views/livewire/crud-form.blade.php
+++ resources/views/livewire/crud-form.blade.php
@@ -1,3 +1,25 @@
+<style>
+    #container {
+        margin-top: 100px;
+    }
+
+</style>
+
 <div>
-    {{-- Stop trying to control. --}}
+    <div id="container" class="container">
+        <div id="row" class="row justify-content-center">
+            <div class="col col-md-6">
+                <div class="mb-3">
+                    <label for="exampleFormControlInput1" class="form-label">Email address</label>
+                    <input type="email" class="form-control" id="exampleFormControlInput1"
+                        placeholder="name@example.com">
+                </div>
+                <div class="mb-3">
+                    <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
+                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                </div>
+                <button type="button" class="btn btn-primary">Submit</button>
+            </div>
+        </div>
+    </div>
 </div>
